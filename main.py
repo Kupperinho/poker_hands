9ac061dda8cd3d8b6195cfacc17c47cb0bd4a529
Kupperinho <adamkupperr@gmail.com>
2021-10-06 10:07:32 +0200

    Making structure more clear and readable. Making improvement in displaying elements: - text in "table" is cleared every time show_hand is called, so every time user sees single text on the screen; -better positioning of elements with grid.

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import random
 from tkinter import *
 
-suits = ('♡', '♢', '♠', '♣')
-ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
-values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
-          '9': 9, 'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
-
+#Card class that allow me to make deck of unique card
 
 class Card:
     def __init__(self, suit, rank):
@@ -15,16 +11,11 @@ class Card:
     def __str__(self):
         return self.rank + self.suit
 
-
-window = Tk()
-window.title("Hand Dealer")
-
-player_info = Canvas(window, width=300, height=300)
-player_info.pack()
-
+#Function which is called after interacting with "deal_button". It makes deck, player hand,
+#his position at the table and shows current efficient stacks
 
 def show_hand():
-    player_info.delete("label1")
+    table.delete("all")
     deck = []
     for suit in suits:
         for rank in ranks:
@@ -34,14 +25,30 @@ def show_hand():
 
     hand = [deck.pop() for n in range(2)]
     positions = ["Btn", "BB", "SB"]
-    random.shuffle(positions)
 
-    label1 = Label(window, text=f'{hand[0]} {hand[1]} pos: {positions[0]} ES:{random.randint(1, 25)}', fg='green', font=('helvetica', 15, 'bold'))
-    player_info.create_window(150, 200, window=label1)
+    player_info = table.create_text(150, 150,
+                                    text=f'{hand[0]} {hand[1]} pos: {random.choice(positions)} ES:{random.randint(1, 25)}',
+                                    fill='white',
+                                    font=('helvetica', 15, 'bold'))
 
+suits = ('♡', '♢', '♠', '♣')
+ranks = ("2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A")
+values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
+          '9': 9, 'T': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
 
-deal_button = Button(text='Deal', command=show_hand, bg='brown', fg='white')
-player_info.create_window(150, 150, window=deal_button)
+#   Interface
+
+window = Tk()
+window.title("Hand Dealer")
+window.config(padx=20, pady=20)
+
+table = Canvas(width=300, height=300, bg="green")
+table.grid(row=0, column=0, columnspan=2, sticky="EW")
+
+deal_button = Button(text='Deal', command=show_hand,  bg="brown", fg="white")
+deal_button.grid(row=1, column=0, columnspan=2)
+
+show_hand()
 
 window.mainloop()
 
